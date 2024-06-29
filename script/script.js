d1513581b409f0f5e1c8bf4156e891bfca6c7100
avento <avento@umich.edu>
2024-06-28 21:46:52 -0400

    major changes to all files

--- script/script.js
+++ script/script.js
@@ -6,48 +6,53 @@ document.addEventListener('DOMContentLoaded', function() {
   const link3 = document.querySelector('a[href="#resume"]');
   const toggler = document.querySelector(".navbar-toggler");
 
-  window.addEventListener('scroll', function() {
-      const top = window.scrollY;
 
-      if (top >= 50) {
-          header.classList.add('navbarDark');
-          // navheader.classList.add('navbarColor');
-          link1.classList.add('navbarLinkColor');
-          link2.classList.add('navbarLinkColor');
-          link3.classList.add('navbarLinkColor');
-      } else {
-          header.classList.remove('navbarDark');
-          // navheader.classList.remove('navbarColor');
-          link1.classList.remove('navbarLinkColor');
-          link2.classList.remove('navbarLinkColor');
-          link3.classList.remove('navbarLinkColor');
-      }
-  });
+  if (header && link1 && link2 && link3) {
+      window.addEventListener('scroll', function() {
+          const top = window.scrollY;
+
+          if (top >= 50) {
+              header.classList.add('navbarDark');
+              link1.classList.add('navbarLinkColor');
+              link2.classList.add('navbarLinkColor');
+              link3.classList.add('navbarLinkColor');
+          } else {
+              header.classList.remove('navbarDark');
+              link1.classList.remove('navbarLinkColor');
+              link2.classList.remove('navbarLinkColor');
+              link3.classList.remove('navbarLinkColor');
+          }
+      });
+  }
 
   const navbarCollapse = document.querySelector('.navbar-expand-lg .navbar-collapse');
 
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 50) {
-          navbarCollapse.classList.add('scrolled');
-      } else {
-          navbarCollapse.classList.remove('scrolled');
-      }
-  });
+  if (navbarCollapse) {
+      window.addEventListener('scroll', function() {
+          if (window.scrollY > 50) {
+              navbarCollapse.classList.add('scrolled');
+          } else {
+              navbarCollapse.classList.remove('scrolled');
+          }
+      });
+  }
 
   const theToggle = document.getElementById('toggle');
 
-  // Function to toggle class
-  function toggleClass(elem, className) {
-      if (elem.classList.contains(className)) {
-          elem.classList.remove(className);
-      } else {
-          elem.classList.add(className);
+  if (theToggle) {
+      // Function to toggle class
+      function toggleClass(elem, className) {
+          if (elem.classList.contains(className)) {
+              elem.classList.remove(className);
+          } else {
+              elem.classList.add(className);
+          }
       }
-  }
 
-  // Click event handler for the toggle button
-  theToggle.onclick = function() {
-      toggleClass(this, 'on');
-      return false;
-  };
+      // Click event handler for the toggle button
+      theToggle.onclick = function() {
+          toggleClass(this, 'on');
+          return false;
+      };
+  }
 });
